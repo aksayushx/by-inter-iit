e881f55bad66e67e0419a9d6e1cff6614ada332c
aksayushx <aksayushx@gmail.com>
2022-03-20 23:33:34 +0530

    minor fix

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
+from utils import Drone,Item
 import pandas as pd
 import numpy as np
 
-def read_data(demands_path="Demand.csv", param_path="Parameters.csv"):
+def read_data(demands_path="Demand.csv"):
   demand = pd.read_csv(demands_path)
-  parameters = pd.read_csv(param_path)
   print(demand.head())
-  print(parameters.head())
 
 
 if __name__ == "__main__":
