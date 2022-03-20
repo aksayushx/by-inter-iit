cf2c87fcb7c86450fd2a249e930390cd5eddaf96
aksayushx <aksayushx@gmail.com>
2022-03-20 23:31:42 +0530

    minor fix

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from email.mime import base
 import pandas as pd
 import numpy as np
 
+def read_data(demands_path="Demand.csv", param_path="Parameters.csv"):
+  demand = pd.read_csv(demands_path)
+  parameters = pd.read_csv(param_path)
+  print(demand.head())
+  print(parameters.head())
+
+
 if __name__ == "__main__":
-    read_data()
+  read_data()
