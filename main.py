4ef66857d4506550fd17ea77b7d6dbbc69533080
Yash Sahijwani <yash.sahijwani.ece19@itbhu.ac.in>
2022-03-22 23:41:16 +0530

    added basecode structure

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
-
 from utils import Drone, Item, Demand, NoFlyZone
 import pandas as pd
 import numpy as np
 from string import ascii_uppercase
 
 # Max Speed
-M=0.0
-#Cost
-C=0.0
-#drone count
-drone_count=[0,0,0,0,0,0]
-#Warehouse Coordinates(x,y,z)
-wh=[]
-#Recharge Stations(x,y,z)
-rhg=[]
-#Drone params P
-P=[0.,0.,0.,0.,0.,0.]
-#Drone Params Q
-Q=[0.,0.,0.,0.,0.,0.]
-#Energy params A
-DA=[0.,0.,0.,0.,0.,0.]
-#Energy parmas B
-DB=[0.,0.,0.,0.,0.,0.]
-#Energy params C
-DC=[0.,0.,0.,0.,0.,0.]
-#Demands
-demands=[]
-#No fly zones
-noflyzones=[]
+M = 0.0
+# Cost
+C = 0.0
+# drone count
+drone_count = [0, 0, 0, 0, 0, 0]
+# Warehouse Coordinates(x,y,z)
+wh = []
+# Recharge Stations(x,y,z)
+rhg = []
+# Drone params P
+P = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+# Drone Params Q
+Q = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+# Energy params A
+DA = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+# Energy parmas B
+DB = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+# Energy params C
+DC = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+# Demands
+demands = []
+# No fly zones
+noflyzones = []
+
+itemtype_objects = []
+
+
+def get_seconds(hhmmss):
+    hh = int(hhmmss.split(":")[0])
+    mm = int(hhmmss.split(":")[1])
+    ss = int(hhmmss.split(":")[2])
+    return (hh - 8) * 3600 + mm * 60 + ss
+
 
 def read_drone_details(path="./data/drone.xlsx", max_speed=10):
     drone_df = pd.read_excel(path)
@@ -41,6 +50,7 @@ def read_drone_details(path="./data/drone.xlsx", max_speed=10):
             drone_df["Payload Capacity (cu.cm)"][i],
             drone_df["Max Slots"],
             max_speed,
+            0,
         )
         drone_objects.append(drone_object)
     return drone_objects
@@ -48,31 +58,29 @@ def read_drone_details(path="./data/drone.xlsx", max_speed=10):
 
 def read_item_details(path="./data/items.xlsx"):
     items_df = pd.read_excel(path)
-    item_objects = {}
     for i in range(items_df.shape[0]):
         item_object = Item(
-            items_df["Item Id"][i],
+            i + 1,
             items_df["Weight (KG)"][i],
             items_df["Length"][i],
             items_df["Breadth"][i],
             items_df["Height"][i],
         )
-        item_objects[items_df["Item Id"][i]] = item_object
-    return item_objects
+        itemtype_objects.append(item_object)
 
 
 def read_demands(demands_path="data/Demand.csv"):
     demand = pd.read_csv(demands_path)
     for index, row in demand.iterrows():
-        warehouse = row["WH"]
+        warehouse = int(row["WH"][-1])
         demand_id = row["Demand ID"]
-        item = row["Item"]
+        item = itemtype_objects[int(row["Item"][-1]) - 1]
         day = row["Day"]
         x = row["X"]
         y = row["Y"]
         z = row["Z"]
-        del_from = row["DeliveryFrom"]
-        del_to = row["DeliveryTo"]
+        del_from = get_seconds(row["DeliveryFrom"])
+        del_to = get_seconds(row["DeliveryTo"])
         failure = row["DeliveryFailure"]
         demands.append(
             Demand(warehouse, demand_id, item, day, x, y, z, del_from, del_to, failure)
@@ -82,6 +90,7 @@ def read_demands(demands_path="data/Demand.csv"):
 def process_params(param_path="data/Parameters.csv"):
 
     parameters = pd.read_csv(param_path)
+    # print(parameters.head())
 
     M = parameters.loc[parameters["Parameter_ID"] == "MaxSpeed (M)", "Value"].iloc[0]
     C = parameters.loc[parameters["Parameter_ID"] == "Cost(C)", "Value"].iloc[0]
@@ -125,65 +134,45 @@ def process_params(param_path="data/Parameters.csv"):
         DC[i - 1] = c
         drone_count[i - 1] = count
 
+    for idx in range(1, 3):
+        zone = []
+        for i in range(1, 9):
+            df = parameters.loc[
+                parameters["Parameter_ID"] == "X" + str(idx) + str(i), "Value"
+            ]
+            if df.empty:
+                break
+            x = parameters.loc[
+                parameters["Parameter_ID"] == "X" + str(idx) + str(i), "Value"
+            ].iloc[0]
+            y = parameters.loc[
+                parameters["Parameter_ID"] == "Y" + str(idx) + str(i), "Value"
+            ].iloc[0]
+            z = parameters.loc[
+                parameters["Parameter_ID"] == "Z" + str(idx) + str(i), "Value"
+            ].iloc[0]
+            zone.append([x, y, z])
+        if len(zone) > 0:
+            noflyzones.append(NoFlyZone(zone))
 
 
-def process_params(param_path="data/Parameters.csv"):
-  
-  parameters=pd.read_csv(param_path)
-  print(parameters.head())
-  
-  M=parameters.loc[parameters['Parameter_ID']=='MaxSpeed (M)','Value'].iloc[0]
-  C=parameters.loc[parameters['Parameter_ID']=='Cost(C)','Value'].iloc[0]
-  
-  for i in range(1,4):
-    df=parameters.loc[parameters['Parameter_ID']=='WH'+str(i)+'X','Value']
-    if(df.empty):
-      break
-    vx=parameters.loc[parameters['Parameter_ID']=='WH'+str(i)+'X','Value'].iloc[0]
-    vy=parameters.loc[parameters['Parameter_ID']=='WH'+str(i)+'Y','Value'].iloc[0]
-    vz=parameters.loc[parameters['Parameter_ID']=='WH'+str(i)+'Z','Value'].iloc[0]
-    wh.append([vx,vy,vz])
-  
-  for i in ascii_uppercase:
-    df=parameters.loc[parameters['Parameter_ID']==i+'X1','Value']
-    if(df.empty):
-      break
-    vx=parameters.loc[parameters['Parameter_ID']==i+'X1','Value'].iloc[0]
-    vy=parameters.loc[parameters['Parameter_ID']==i+'Y1','Value'].iloc[0]
-    rhg.append([vx,vy,0.0])
+def check_availibility(drone):
+    pass
 
-  for i in range(1,7):
-    p=parameters.loc[parameters['Parameter_ID']=='P'+str(i),'Value'].iloc[0]
-    q=parameters.loc[parameters['Parameter_ID']=='Q'+str(i),'Value'].iloc[0]
-    a=parameters.loc[parameters['Parameter_ID']=='A'+str(i),'Value'].iloc[0]
-    b=parameters.loc[parameters['Parameter_ID']=='B'+str(i),'Value'].iloc[0]
-    c=parameters.loc[parameters['Parameter_ID']=='C'+str(i),'Value'].iloc[0]
-    count=parameters.loc[parameters['Parameter_ID']=='DT'+str(i)+'Count','Value'].iloc[0]
-    P[i-1]=p
-    Q[i-1]=q
-    DA[i-1]=a
-    DB[i-1]=b
-    DC[i-1]=c
-    drone_count[i-1]=count
 
-  for idx in range(1,3):
-    zone=[]
-    for i in range(1,9):
-      df=parameters.loc[parameters['Parameter_ID']=='X'+str(idx)+str(i),'Value']
-      if df.empty:
-        break
-      x=parameters.loc[parameters['Parameter_ID']=='X'+str(idx)+str(i),'Value'].iloc[0]
-      y=parameters.loc[parameters['Parameter_ID']=='Y'+str(idx)+str(i),'Value'].iloc[0]
-      z=parameters.loc[parameters['Parameter_ID']=='Z'+str(idx)+str(i),'Value'].iloc[0]
-      zone.append([x,y,z])
-    if len(zone)>0:
-      noflyzones.append(NoFlyZone(zone))
-    
+def check_weight_volume(drone, item):
+    if item.volume <= drone.payload_volume and item.weight <= drone.payload_weight:
+        return 1
+    else:
+        return 0
+
+def calculate_starting_time(drone)
+
 
 if __name__ == "__main__":
+    read_item_details()
     process_params()
     read_demands()
-    # max_speed and drone counts will come from parameters
     dronetype_objects = read_drone_details(max_speed=M)
     drones = []
     for i in range(len(drone_count)):
@@ -196,6 +185,21 @@ if __name__ == "__main__":
                 dronetype_objects[i].payload_volume,
                 dronetype_objects[i].slots,
                 dronetype_objects[i].max_speed,
+                j,
             )
             drones.append(drone_object)
-    itemtype_objects = read_item_details()
+
+    demands.sort(key=lambda x: x.del_to, reverse=True)
+    for demand in demands:
+        # processing each demand
+        demand_item = demand.item
+        for drone in drones:
+            possible = check_weight_volume(demand_item, drone)
+            if not possible:
+                continue
+            startpoint = wh[demand.wh - 1]
+            endpoint = [demand.x, demand.y, demand.z]
+            # if both satisfied, then assign drone to demand, and then generate path
+            timestamp = calculate_starting_time(drone, path)
+
+            check_battery_feasibility(drone, path)
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from cv2 import inRange
 import pandas as pd
 import numpy as np
+from string import ascii_uppercase
 
 
 class Drone:
@@ -13,6 +14,7 @@ class Drone:
         payload_volume,
         slots,
         max_speed,
+        id,
     ):
 
         self.type = type
@@ -22,6 +24,7 @@ class Drone:
         self.payload_volume = payload_volume
         self.slots = slots
         self.max_speed = max_speed
+        self.id = 'D' + str(self.type) + ascii_uppercase[id]
 
         self.current_charge = battery_capacity
         self.current_weight = base_weight
@@ -31,6 +34,28 @@ class Drone:
         self.x = 0
         self.y = 0
         self.z = 0
+
+        self.occupied_slots = []
+
+    def occupy_update(self, starting_time, ending_time):
+
+        self.occupied_slots.append([starting_time, ending_time])
+
+    def check_occupy(self, timestamp):
+
+        for i in range(len(self.occupied_slots)):
+            occupied = (
+                1
+                if (
+                    timestamp >= self.occupied_slots[i][0]
+                    and timestamp <= self.occupied_slots[i][1]
+                )
+                else 0
+            )
+            if occupied:
+                return 1
+
+        return 0
 
 
 class Item:
@@ -44,74 +69,54 @@ class Item:
         self.volume = self.l * self.b * self.h
 
 
-
 class Demand:
-    def __init__(self,wh,demand_id,item,day,x,y,z,del_from,del_to,failure):
+    def __init__(self, wh, demand_id, item, day, x, y, z, del_from, del_to, failure):
 
-        self.wh=wh
-        self.demand_id=demand_id
-        self.item=item
-        self.day=day
-        self.x=x
-        self.y=y
-        self.z=z
-        self.del_from=del_from
-        self.del_to=del_to
-        self.failure=failure
+        self.wh = wh
+        self.demand_id = demand_id
+        self.item = item
+        self.day = day
+        self.x = x
+        self.y = y
+        self.z = z
+        self.del_from = del_from
+        self.del_to = del_to
+        self.failure = failure
 
 
 class NoFlyZone:
-    def __init__(self,points) -> None:
-        self.points=points
-        self.mn=np.min(np.array(points),axis=0)
-        self.mx=np.max(np.array(points),axis=0)
+    def __init__(self, points) -> None:
+        self.points = points
+        self.mn = np.min(np.array(points), axis=0)
+        self.mx = np.max(np.array(points), axis=0)
         print(self.mx)
         print(self.mn)
-    
-    def inRange(self,a,b,c):
-        return c>=a and c<=b
 
-    def doesIntersect(self,a,b):
-        if a[0]==b[0]:
-            return inRange(self.mn[0],self.mx[0],a[0])
-        if a[1]==b[1]:
-            return inRange(self.mn[1],self.mn[1],a[1])
-            
-        # Put x 
-        y=a[1]+((b[1]-a[1])*(self.mn[0]-a[0]))/(b[0]-a[0])
-        if inRange(self.mn[1],self.mx[1],y):
+    def inRange(self, a, b, c):
+        return c >= a and c <= b
+
+    def doesIntersect(self, a, b):
+        if a[0] == b[0]:
+            return inRange(self.mn[0], self.mx[0], a[0])
+        if a[1] == b[1]:
+            return inRange(self.mn[1], self.mn[1], a[1])
+
+        # Put x
+        y = a[1] + ((b[1] - a[1]) * (self.mn[0] - a[0])) / (b[0] - a[0])
+        if inRange(self.mn[1], self.mx[1], y):
             return True
 
-        y=a[1]+((b[1]-a[1])*(self.mx[0]-a[0]))/(b[0]-a[0])
-        if inRange(self.mn[1],self.mx[1],y):
+        y = a[1] + ((b[1] - a[1]) * (self.mx[0] - a[0])) / (b[0] - a[0])
+        if inRange(self.mn[1], self.mx[1], y):
             return True
 
         # Put y
-        x=a[0]+((b[0]-a[0])*(self.mn[1]-a[1]))/(b[1]-a[1])
-        if inRange(self.mn[0],self.mx[0],x):
+        x = a[0] + ((b[0] - a[0]) * (self.mn[1] - a[1])) / (b[1] - a[1])
+        if inRange(self.mn[0], self.mx[0], x):
             return True
 
-        x=a[0]+((b[0]-a[0])*(self.mx[1]-a[1]))/(b[1]-a[1])
-        if inRange(self.mn[0],self.mx[0],x):
+        x = a[0] + ((b[0] - a[0]) * (self.mx[1] - a[1])) / (b[1] - a[1])
+        if inRange(self.mn[0], self.mx[0], x):
             return True
 
         return False
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
